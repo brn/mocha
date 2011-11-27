834214904bdb3342e9a7946dcb666f76d0f5d567
Taketoshi Aono <dobaw20@gmail.com>
2011-11-27 17:30:58 +0900

    remove files.

--- src/mkdir.h
+++
@@ -1,8 +0,0 @@
-#ifndef mocha_mkdir_h_
-#define mocha_mkdir_h_
-
-namespace mocha {
-bool Mkdir( const char* path , int permiss );
-}
-
-#endif
--- src/platform.h
+++
@@ -1,8 +0,0 @@
-
-#if defined __GNUC__
-#include "platform_linux.h"
-#elif defined _WIN32
-#include "platform_win.h"
-#endif
-
-
