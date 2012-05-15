e4fe5119ac0e4385f763e72c968b904eb9bb4d40
Taketoshi Aono <dobaw20@gmail.com>
2012-05-15 22:33:05 +0900

    fix the links.

--- files/js/source/index.js
+++ files/js/source/index.js
@@ -8,6 +8,20 @@ imageCache.cache(['images/stripes.gif',
                   'images/logo.png',
                   'images/bg2.jpg']);
 
+class BinaryLinks {
+  constructor(@_binLink = $('#download')){}
+  public addEvent() {
+    var {oscpu} = navigator;
+    if (oscpu.indexOf('Windows') > -1) {
+      @_binLink.on('click', -> location.href = '../bin/win32/mocha.zip');
+    } else if (oscpu.indexOf('Mac') > -1) {
+      @_binLink.on('click', -> location.href = '../bin/macos/mocha.zip');
+    } else {
+      @_binLink.on('click', -> alert('Sorry, the binary file is not provided for your os. Please build from the sources.'));
+    }
+  }
+};
+
 $(->{
   var filterBug = /MSIE [678]/.test(navigator.userAgent);
   if filterBug {
@@ -21,4 +35,6 @@ $(->{
     setTimeout(->index.LINE_CACHE().fadeIn(2000), 2500);
     index.BG_CACHE().fadeIn(3000);
   }
+  var binaryLinks = new BinaryLinks;
+  binaryLinks.addEvent();
 })
